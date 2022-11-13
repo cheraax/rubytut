8d3359ac7dbf27adb87ad63128c960dda9dbec49
cheraax <andrey@chernov.pw>
2022-11-13 14:52:14 +0300

    lesson 9

--- lesson9/9-1.rb
+++ lesson9/9-1.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 temperature = ARGV[0]
 season = ARGV[1]
 if temperature.nil? || season.nil?
-    print 'Какая сейчас температура?: '
-    temperature = STDIN.gets
-    print 'Какое сейчас время года?: '
-    season = STDIN.gets
+  print 'Какая сейчас температура?: '
+  temperature = $stdin.gets
+  print 'Какое сейчас время года?: '
+  season = $stdin.gets
 end
 temperature = temperature.to_f
 season = season.to_i
-if temperature.between?(22,99) || 
-   (season == 1 && temperature.between?(15,35))
-   puts 'Скорре в парк, слушать соловьев!'
+if temperature.between?(22, 99) ||
+   (season == 1 && temperature.between?(15, 35))
+  puts 'Скорре в парк, слушать соловьев!'
 else
-    puts 'Сейчас холодно и соловьи греются!'
+  puts 'Сейчас холодно и соловьи греются!'
 end
